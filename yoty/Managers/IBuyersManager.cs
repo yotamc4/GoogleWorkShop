302babd42f486097c7d115f8d37eb9223e4707b9
Tzachi Hakmon <t-tzhakm@microsoft.com>
2020-11-11 18:23:08 +0200

    start

---
+++ yoty/Managers/IBuyersManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YOTY.Service.WebApi.PublicDataSchemas;
+
+namespace YOTY.Service.Managers
+{
+    public interface IBuyersManager
+    {
+        Task<IList<Buyer>> GetBuyers(IList<string> buyersIds);
+        Task<IList<Buyer>> GetBuyers(string productBidId);
+
+    }
+}
