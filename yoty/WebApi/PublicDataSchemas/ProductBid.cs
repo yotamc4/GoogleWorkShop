25fc548edbd2b18a20b81ae5dc982f6493b25ae0
Tzachi Hakmon <t-tzhakm@microsoft.com>
2020-11-11 18:02:27 +0200

    public schemas

---
+++ yoty/WebApi/PublicDataSchemas/ProductBid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YOTY.Service.WebApi.PublicDataSchemas
+{
+    // data structure represents product bit with crud
+    public class ProductBid
+    {
+        string Id { get; set; }
+
+        string Name { get; set; }
+
+        string OwnerId { get; set; }
+
+        string Category { get; set; }
+
+        double MaxPrice { get; set; }
+
+        DateTime CreationDate { get; set; }
+
+        DateTime ExpirationDate { get; set; }
+
+        List<Uri> ProductImages {get;set;}
+
+        string Description { get; set; }
+
+        int PotenialSuplliersCounter { get; set; }
+
+        int UnitsCounter { get; set; }
+
+
+    }
+}
