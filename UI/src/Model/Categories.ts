af4c33423c0763d7f756ae9e10acc9698f537195
Yonatan Bouskila <t-yobous@microsoft.com>
2020-11-12 14:15:22 +0200

    Add navigation pane

---
+++ UI/src/Model/Categories.ts
@@ -0,0 +1,33 @@
+export const Categories: Map<string, string[]> = new Map();
+
+Categories.set("Electorincs", [
+  "Cell Phones",
+  "Headphones",
+  "Television",
+  "Media Streamer",
+]);
+
+Categories.set("Computers", ["Computers", "Laptops", "Monitors", "Tablets"]);
+
+Categories.set("Transportations", [
+  "Bicycle",
+  "Electronic scooters",
+  "Electric bicycle",
+  "Segway",
+]);
+
+Categories.set("ElectorincsB", [
+  "Cell Phones",
+  "Headphones",
+  "Television",
+  "Media Streamer",
+]);
+
+Categories.set("ComputersB", ["Computers", "Laptops", "Monitors", "Tablets"]);
+
+Categories.set("TransportationsB", [
+  "Bicycle",
+  "Electronic scooters",
+  "Electric bicycle",
+  "segway",
+]);
---
+++ UI/src/NavigationPane/NavigationPane.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Nav, INavLinkGroup } from "office-ui-fabric-react/lib/Nav";
+import { INavLink } from "@fluentui/react";
+import { Categories } from "../Model/Categories";
+
+function createINavLinkFromString(name: string): INavLink {
+  return {
+    name: name,
+    url: `http://example.com/${name}`,
+    target: "_blank",
+  };
+}
+
+function navLinkGroups(map: Map<string, string[]>): INavLinkGroup[] {
+  let links: INavLink[] = [];
+  map.forEach((value: string[], key: string) => {
+    links.push({
+      name: key,
+      url: `http://example.com/${key}`,
+      target: "_blank",
+      links: value.map(createINavLinkFromString),
+    });
+  });
+
+  return [
+    {
+      links: links,
+    },
+  ];
+}
+
+export const NavigationPane: React.FunctionComponent = () => {
+  return (
+    <Nav ariaLabel="Nav with nested links" groups={navLinkGroups(Categories)} />
+  );
+};
