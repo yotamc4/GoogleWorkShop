1477965d1064de7814847b575663d58971d18385
Yonatan Bouskila <t-yobous@microsoft.com>
2020-11-19 13:37:46 +0200

    update product page

--- UI/src/ProductPage/ProductPageStyles.tsx
+++ UI/src/ProductPage/ProductPageStyles.tsx
@@ -1,4 +1,10 @@
-import { FontWeights, IDropdownStyles, ITextStyles } from "@fluentui/react";
+import {
+  FontWeights,
+  IDropdownStyles,
+  ITextStyles,
+  mergeStyles,
+  mergeStyleSets,
+} from "@fluentui/react";
 
 export const inputWidth: string = "100%";
 
@@ -6,12 +12,16 @@ export const headerStyle: ITextStyles = {
   root: { fontWeight: FontWeights.regular },
 };
 
+export const descriptionStyle: ITextStyles = {
+  root: { maxWidth: "35rem" },
+};
+
 export const dropdownStyles: Partial<IDropdownStyles> = {
   dropdown: { width: inputWidth },
 };
 
 export const verticalGapStackTokens = {
-  childrenGap: 10,
+  childrenGap: 10000,
   padding: 10,
 };
 
@@ -24,3 +34,14 @@ export const imageStyles = {
   childrenGap: "20rem",
   padding: 5,
 };
+
+const iconClass = mergeStyles({
+  fontSize: 50,
+  height: 50,
+  width: 50,
+  margin: "0 25px",
+});
+
+export const classNames = mergeStyleSets({
+  greenYellow: [{ color: "greenyellow" }, iconClass],
+});
