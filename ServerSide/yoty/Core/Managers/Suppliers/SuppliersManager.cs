98bda374225b20a5c4aa90e5bb12a671a5711dda
Yotam Cohen <t-yocoh@microsoft.com>
2020-12-09 17:42:26 +0200

    cont

---
+++ ServerSide/yoty/Core/Managers/Suppliers/SuppliersManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YOTY.Service.WebApi.PublicDataSchemas;
+
+namespace YOTY.Service.Core.Managers.Suppliers
+{
+    public class SuppliersManager : ISuppliersManager
+    {
+        public Task<Response<SupplierDTO>> CreateSupplier(NewSupplierRequest newSupplierRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> DeleteSupplier(string buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<SupplierDTO>> GetSupplier(string buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BidDTO>> GetSupplierDeals(string buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BidDTO>> GetSupplierLiveBids(string buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BidDTO>> GetSupplierOldBids(string buyerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<SupplierDTO>> ModifySupplierDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
