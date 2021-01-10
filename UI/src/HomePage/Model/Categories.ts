dee69619447383d060759397d3d314e84e8c40cd
Yotam Cohen <t-yocoh@microsoft.com>
2021-01-10 17:16:28 +0200

    re-arrange categories

--- UI/src/HomePage/Model/Categories.ts
+++ UI/src/HomePage/Model/Categories.ts
@@ -1,26 +1,12 @@
 export const CategoriesMap: Map<string, string[]> = new Map();
 
-CategoriesMap.set("Appliances", [
-    "Kitchen Appliances",
-    "Washers & Dryers",
-    "Vacuums & Floor Care",
-    "Heating, Cooling & Air Quality",
-]);
 
-CategoriesMap.set("Audio", [
-    "Home Audio",
-    "Headphones",
-    "Bluetooth Speakers",
-    "Car Audio",
-    "Musical Instruments",
-]);
-
-CategoriesMap.set("Cameras, Camcorders & Drones", [
-    "Cameras & Lenses",
-    "Camcorders",
-    "Vlogging Gear",
-    "Drones",
-    "Accessories",
+CategoriesMap.set("Electronics", [
+    "Car & Vehicle Electronics",
+    "Cell Phones & Accessories",
+    "Gaming",
+    "Smart Watches",
+    "Other"
 ]);
 
 CategoriesMap.set("Computers & Tablets", [
@@ -34,12 +20,43 @@ CategoriesMap.set("Computers & Tablets", [
     "Accessories",
 ]);
 
-CategoriesMap.set("Electronics", [
-    "Car & Vehicle Electronics",
-    "Cell Phones & Accessories",
-    "Gaming",
-    "Smart Watches",
-    "Other"
+CategoriesMap.set("Audio", [
+    "Home Audio",
+    "Headphones",
+    "Bluetooth Speakers",
+    "Car Audio",
+    "Musical Instruments",
+]);
+
+
+CategoriesMap.set("Transportation", [
+    "Bicycles",
+    "Electric scooters",
+    "Electric bikes",
+    "Segway",
+]);
+
+
+CategoriesMap.set("TV & Home Theater", [
+    "TVs",
+    "Home Theater Audio & Video",
+    "Media Streamers",
+    "Accessories",
+]);
+
+CategoriesMap.set("Appliances", [
+    "Kitchen Appliances",
+    "Washers & Dryers",
+    "Vacuums & Floor Care",
+    "Heating, Cooling & Air Quality",
+]);
+
+CategoriesMap.set("Cameras, Camcorders & Drones", [
+    "Cameras & Lenses",
+    "Camcorders",
+    "Vlogging Gear",
+    "Drones",
+    "Accessories",
 ]);
 
 CategoriesMap.set("Health, Fitness & Personal Care", [
@@ -61,18 +78,4 @@ CategoriesMap.set("Smart Home, Security & Wi-Fi", [
     "Smart Home",
     "Home Security & Monitoring",
     "Wi-Fi & Networking",
-]);
-
-CategoriesMap.set("Transportation", [
-    "Bicycles",
-    "Electric scooters",
-    "Electric bikes",
-    "Segway",
-]);
-
-CategoriesMap.set("TV & Home Theater", [
-    "TVs",
-    "Home Theater Audio & Video",
-    "Media Streamers",
-    "Accessories",
 ]);
